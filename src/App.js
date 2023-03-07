338b2eea0525fa7d28bd3ab1614f22d936983376
Ismail Hasan <ismailhasan0132@gmail.com>
2023-03-06 23:50:53 -0800

    added react.jsx

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React.js
+          Learn React.jsx
         </a>
       </header>
     </div>
