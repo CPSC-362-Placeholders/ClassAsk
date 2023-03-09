f75fc95b43e26b4a9564ed5e59204d98e4417465
Ismail Hasan <ismailhasan0132@gmail.com>
2023-03-08 20:13:57 -0800

    added t

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React.jsx
+          Learn React.jsxt
         </a>
       </header>
     </div>
