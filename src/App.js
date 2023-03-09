4998cd256b7ccad761ef0aa8c65ae998555083b8
QuillJoy <jwquill81@gmail.com>
2023-03-08 20:27:45 -0800

    add lol

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React.jsxt
+          Learn React.jsxtlol
         </a>
       </header>
     </div>
