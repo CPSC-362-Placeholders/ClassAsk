f6e876a1ac452953cc1d22538e57bf068f6a5009
Ahmir-Bijanzadeh <ahmirbijan@yahoo.com>
2023-03-10 17:11:04 -0800

    small update to login page
    checkbox missing /> and js version matches the work done in the index.html

--- src/Login.js
+++ src/Login.js
@@ -6,10 +6,12 @@ function Login()
               Login to your ClassAsk Account!
           </h1> 
           <form>
-          <input type="email" placeholder="CSUF Email" />
-		    	<input type="password" placeholder="Password" />
-			    <a href="#">Forgot your password?</a>
-			    <button>Sign In</button>
+                <input type="email" placeholder="CSUF Email" /><br></br>
+		    	<input type="password" placeholder="Password" /><br></br>
+                <button>Sign In</button><br></br>
+                <input type="checkbox"/> Remember Me<br></br>
+			    <a href="#">Forgot your password?</a><br></br>
+                <a href="#">Register For an Account</a>
           </form>
       </div>
   )
