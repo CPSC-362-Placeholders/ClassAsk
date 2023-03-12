e36b7be81bb5e18ff280712ffadcb2efb9c8da04
Ahmir-Bijanzadeh <ahmirbijan@yahoo.com>
2023-03-12 01:23:33 -0800

    added login stuff after properly adding it with node

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Register from './Register.js';
+import Login from './Login.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
     <Register />
+    <Login />
   </React.StrictMode>
 );
 
