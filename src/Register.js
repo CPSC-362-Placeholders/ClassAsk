627addf2511981e17dbabdc2193071d6bec74a45
Ismail Hasan <ismailhasan0132@gmail.com>
2023-03-16 01:08:36 -0700

    added register.php, connected react to my local db using php

--- src/Register.js
+++ src/Register.js
@@ -1,27 +1,51 @@
+import $ from 'jquery';
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
 function Register(){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    const handleSubmit = () => {
+        register(email, password);
+        navigate('/');
+    }
     return (
         <div>
             <h1>
                 Create a ClassAsk Account
             </h1> 
             <form>
-                <label for="fname" >First Name:</label>
-                <input type="text" id="fname" firstName="fname" required /> <br/><br/>
+                <label>First Name:</label>
+                <input type="text" id="fname" required /> <br/><br/>
 
-                <label for="lname">Last Name:</label>
-                <input type="text" id="lname" lastName="lname" required /> <br/><br/>
+                <label>Last Name:</label>
+                <input type="text" id="lname" required /> <br/><br/>
         
-                <label for="email">CSUF Email:</label>
-                <input type="email" id="email" email="email" required/> <br/><br/>
+                <label>CSUF Email:</label>
+                <input type="email" id="email" onChange={(e) => setEmail(e.target.value)} required/> <br/><br/>
 
-                <label for="password">Set Password:</label>
-                <input type="password" id="password" password="password" required minlength="8" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"/> <br/>
+                <label>Set Password:</label>
+                <input type="password" id="password" onChange={(e) => setPassword(e.target.value)}  required minLength="8" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"/> <br/>
                 <p>  (Password should have at least 1 digit, 1 lowercase letter, 1 uppercase letter, and have a minumum of 8 characters) </p>
 
-                <button type="submit"> Submit </button>
+                <button type="submit" onClick={() => handleSubmit()}> Submit </button>
             </form>
         </div>
     )
+}
+function register(email, password) {
+    $.ajax({
+        url: 'http://localhost/classask/src/php/register.php',
+        type: 'GET',
+        data: {email: email, password: password},
+        success: function (data) {
+            console.log(data);
+        },
+        error: function () {
+            alert("error");
+        }
+    });
 }
 
 export default Register;
