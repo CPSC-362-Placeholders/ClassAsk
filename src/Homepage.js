338d033627093d46c68cb7c1191f85759f99280a
Ismail Hasan <ismailhasan0132@gmail.com>
2023-04-14 23:57:56 -0700

    moved the subscribe functionality into it's own component

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,93 +1,30 @@
-import $ from "jquery";
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import {useNavigate} from "react-router-dom";
 
-
-function Homepage(){
+function Homepage() {
     let session = window.sessionStorage.getItem("session");
     let parsed_session = JSON.parse(session);
     let name = parsed_session.first_name + " " + parsed_session.last_name;
-    let email = parsed_session.email;
+    const navigate = useNavigate();
 
-
-    const [courseList, setCourseList] = useState();
-    const [subscribe, setSubscribe] = useState();
-
-
-
-    var fetchClassList = () => {
-        $.ajax({
-            url: 'http://localhost/classask/src/php/fetchClassList.php',
-            type: 'GET',
-            async: false,
-            success: function (data) {
-                setCourseList(data);  //console.log(data);
-            },
-            error: function (err) {
-                console.log(err);
-            }
-        })
+    const subscribe = () => {
+        navigate('/subscribe');
+        window.location.reload();
     }
-    var subClass = () => {
-        console.log(email);
-        if(subscribe === undefined || subscribe === "classPlaceHolder"){
-            alert("invalid class selection");
-            return;
-        }
-        $.ajax({
-            url: 'http://localhost/classask/src/php/subscribe.php',
-            type: 'POST', //modifys data 
-            async: false,
-            data: {name:subscribe, email:email},
-            success: function (data) {
-                console.log(data);
-            },
-            error: function (err) {
-                console.log(err);
-            }
-        })
-        
-    }
-
-    useEffect(()=>{
-        fetchClassList();  
-    },[]);
-
-    //courselist dropdown population
-    useEffect(()=>{ 
-    if (courseList != null){
-        const dropdown = document.getElementById("classDropdown");
-        var array = JSON.parse(courseList); //checks the string and parses it into an object we can use
-        var convertArray = Object.values(array); //object is being converted to an array each value
-        convertArray.forEach(element => { // for each value named elemnt in the array do work that creates a new element (option) which are then set to the element, which then adds a new line to the dropdown
-            const option = document.createElement("option");
-            option.value = element;
-            option.text = element;
-            dropdown.appendChild(option);
-        });
-    }
-    },[courseList])
 
     return (
         <>
-        <div>
-            <h1>
-            <label>Welcome to ClassAsk!</label> <br></br>
-                {name}
-                <br></br>
-                <select name="classDropdown" id="classDropdown" onChange={(e)=> setSubscribe(e.target.value)} >
-                    <option value="classPlaceHolder">choose a class</option>
-                </select>
-                <br></br>
-                <button type="submit" name="saveSelect" onClick={() => subClass()}>Subscribe</button>
-                 
-                
-            </h1>
-            
-        </div>
+            <div>
+                <h1>
+                    <label>Welcome to ClassAsk!</label> <br></br>
+                    {name}
+                </h1>
+                <button onClick={() => subscribe()}>Subscribe to a course!</button>
+            </div>
         </>
-        
     );
 
 }
+
 export default Homepage;
 
---
+++ src/Subscribe.js
@@ -0,0 +1,88 @@
+import $ from "jquery";
+import React, {useState, useEffect} from 'react';
+import {useNavigate} from "react-router-dom";
+
+function Subscribe() {
+    let session = window.sessionStorage.getItem("session");
+    let parsed_session = JSON.parse(session);
+    let email = parsed_session.email;
+
+    const [subscribe, setSubscribe] = useState();
+    const [courseList, setCourseList] = useState();
+
+    const navigate = useNavigate();
+
+    const subscribeToClass = () => {
+        console.log(email);
+        if (subscribe === undefined || subscribe === "classPlaceHolder") {
+            alert("invalid class selection");
+            return;
+        }
+        $.ajax({
+            url: 'http://localhost/classask/src/php/subscribe.php',
+            type: 'POST', //modifies data
+            async: false,
+            data: {name: subscribe, email: email},
+            success: function (data) {
+                if (data === "subscribed successfully") {
+                    alert("Subscribed to course successfully!");
+                    navigate("/homepage");
+                    window.location.reload();
+                }
+            },
+            error: function (err) {
+                console.log(err);
+            }
+        })
+    };
+
+    const fetchClassList = () => {
+        $.ajax({
+            url: 'http://localhost/classask/src/php/fetchClassList.php',
+            type: 'GET',
+            async: false,
+            success: function (data) {
+                setCourseList(data);  //console.log(data);
+            },
+            error: function (err) {
+                console.log(err);
+            }
+        })
+    };
+
+    useEffect(() => {
+        fetchClassList();
+    }, []);
+
+    // courselist dropdown population
+    useEffect(() => {
+        if (courseList != null) {
+            const dropdown = document.getElementById("classDropdown");
+            const array = JSON.parse(courseList); //checks the string and parses it into an object we can use
+            const convertArray = Object.values(array); //object is being converted to an array each value
+            convertArray.forEach(element => { // for each value named element in the array do work that creates a new element (option) which are then set to the element, which then adds a new line to the dropdown
+                const option = document.createElement("option");
+                option.value = element;
+                option.text = element;
+                dropdown.appendChild(option);
+            });
+        }
+    }, [courseList])
+
+    return (
+        <div>
+            <h1>
+                Subscribe to a course
+            </h1>
+            <select name="classDropdown" id="classDropdown" onChange={(e) => setSubscribe(e.target.value)}>
+                <option value="classPlaceHolder">choose a class</option>
+            </select>
+            <br/>
+            <br/>
+            <button type="submit" onClick={() => subscribeToClass()}>Subscribe</button>
+        </div>
+    );
+
+}
+
+export default Subscribe;
