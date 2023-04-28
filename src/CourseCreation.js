0ea708f2f974440669ad7cd5417f9d839ff2cb36
QuillJoy <jwquill81@gmail.com>
2023-04-27 17:10:53 -0700

    User input rejected if course already exists

--- src/CourseCreation.js
+++ src/CourseCreation.js
@@ -45,11 +45,14 @@ function create(className, classCode, classNumber) {
         type: 'POST',
         data: {className: className, classCode: classCode, classNumber: classNumber},
         success: function (data) {
-            console.log(data);
-            alert("Successfully created course!");
+            if (data === "Already created") {
+                alert("A section already exists for that class! Find it in the subscribe drop down list.");
+            } else{
+                alert("Successfully created class. Find it in the subcribe drop down list.");
+            }
         },
-        error: function () {
-            alert("error");
+        error: function (err) {
+            console.log(err);
         }
     });
 }
--- src/php/courseCreation.php
+++ src/php/courseCreation.php
@@ -5,8 +5,16 @@ if(isset($_POST['className']) && $_POST['classCode'] && isset($_POST['classNumbe
     $classCode = $_POST['classCode'];
     $classNumber = $_POST['classNumber'];
 
-    $query = "INSERT INTO classlist (class_name, class_code, section_number, subject_name, semester) VALUES ('$className', '$classCode', '$classNumber', ' ',' ')";
-    mysqli_query($conn, $query);
-    echo "done";
+    $checkQuery = "SELECT * FROM classlist WHERE class_name='$className' AND class_code='$classCode' AND section_number='$classNumber'";
+    $checkResult = mysqli_query($conn, $checkQuery);
+    $count = mysqli_num_rows($checkResult);
+    
+    if ($count != 0) {
+        echo "Already created";
+        return;
+    } else {
+        $query = "INSERT INTO classlist (class_name, class_code, section_number, subject_name, semester) VALUES ('$className', '$classCode', '$classNumber', ' ',' ')";
+        mysqli_query($conn, $query);
+    }
 }
 
