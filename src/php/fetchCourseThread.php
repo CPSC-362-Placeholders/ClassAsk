4f9b96e6828ffd4423f970db174e7543f8dcc38e
Ismail Hasan <ismailhasan0132@gmail.com>
2023-05-05 13:32:08 -0700

    Finished CLAS-48

--- src/php/fetchCourseThread.php
+++ src/php/fetchCourseThread.php
@@ -2,7 +2,7 @@
 include 'connect.php';
 
 $courseId = $_POST['id'];
-$query = "SELECT class_name FROM classlist WHERE section_number='$courseId'";
+$query = "SELECT class_name FROM classlist WHERE class_code='$courseId'";
 $result = mysqli_query($conn, $query);
 
 if($result) {
