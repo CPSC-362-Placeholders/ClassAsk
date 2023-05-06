d00fc0e7f163f50ba7b14c3c79089184da94bf32
Ismail Hasan <ismailhasan0132@gmail.com>
2023-05-05 17:10:25 -0700

    Finished CLAS-27 and CLAS-30

--- src/Post.js
+++ src/Post.js
@@ -10,6 +10,9 @@ function Post() {
     const postId = queryParams.get("id");
     const [postTitle, setPostTitle] = useState();
     const [postDescription, setPostDescription] = useState();
+    const [replyArea, setReplyArea] = useState();
+    const [replies, setReplies] = useState();
+    const [repliesComponent, setRepliesComponent] = useState();
 
 
     const getPostTitle = () => {
@@ -17,7 +20,7 @@ function Post() {
             url: 'http://localhost/classask/src/php/getPostTitle.php',
             type: 'GET',
             async: false,
-            data: {id:postId},
+            data: {id: postId},
             success: function (data) {
                 setPostTitle(data);
             },
@@ -32,7 +35,7 @@ function Post() {
             url: 'http://localhost/classask/src/php/getPostDescription.php',
             type: 'GET',
             async: false,
-            data: {id:postId},
+            data: {id: postId},
             success: function (data) {
                 setPostDescription(data);
             },
@@ -42,10 +45,58 @@ function Post() {
         });
     };
 
+    const getReplies = () => {
+        $.ajax({
+            url: 'http://localhost/classask/src/php/getReplies.php',
+            type: 'GET',
+            async: false,
+            data: {id: postId},
+            success: function (data) {
+                setReplies(data);
+            },
+            error: function (err) {
+                console.log(err);
+            }
+        });
+    }
+
+    const postReply = (replyToId, baseThreadId) => {
+        $.ajax({
+            url: 'http://localhost/classask/src/php/createNewReply.php',
+            type: 'POST',
+            async: false,
+            data: {reply: replyArea, replyToId: replyToId, baseThreadId: baseThreadId},
+            success: function (data) {
+                console.log(data);
+            },
+            error: function (err) {
+                console.log(err);
+            }
+        });
+        window.location.reload();
+    };
+
     useEffect(() => {
         getPostTitle();
         getPostDescription();
-    }, [])
+        getReplies();
+    }, []);
+
+    useEffect(() => {
+        if (replies !== undefined) {
+            let parsedReplies = JSON.parse(replies);
+            let html = [];
+            for (let i = 0; i < parsedReplies.length; ++i) {
+                html.push(
+                    <div>
+                        <p style={{marginLeft: 1 + 'em'}}>{parsedReplies[i]}</p>
+                        <button style={{marginLeft: 1 + 'em'}}>Reply..</button>
+                    </div>
+                );
+            }
+            setRepliesComponent(html);
+        }
+    }, [replies]);
 
 
     return (
@@ -54,9 +105,13 @@ function Post() {
             <br/>
             <p>{postDescription}</p>
             <br/>
-            <textarea rows={4} cols={30}/>
+            <textarea rows={4} cols={30} onChange={(e) => setReplyArea(e.target.value)}/>
             <br/>
-            <button>Reply...</button>
+            <button onClick={() => postReply(postId, postId)}>Reply...</button>
+            <br/><br/>
+            <div>
+                {repliesComponent}
+            </div>
         </div>
     )
 }
